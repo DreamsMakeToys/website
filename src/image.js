757fd5f6c94be390d36419cc4cb75056f163ee0b
Jared Mathews <dreamsmaketoys@gmail.com>
2018-06-13 12:51:13 -0600

    progress pulling out image component & cleanup

---
+++ src/image.js
@@ -0,0 +1,128 @@
+import { Component } from 'inferno'
+import Styles from './image.css'
+
+var IMAGE_RESOLUTION = 5
+
+function View(props) {
+  var size = null
+  var content = null
+  if (props.rect) {
+    var { left, top, width } = props.rect
+    var size = { width, height: width }
+    var rect = { left, top, width, height: width }
+    var imageClassName = `${Styles.image} ${
+      props.isLoading ? Styles.hidden : ''
+    }`
+    var tiles = createTiles(props.name, IMAGE_RESOLUTION, props.onTileLoad)
+    content = [
+      <div className={Styles.spinner} style={rect}>
+        <div class={Styles.laBallScaleMultiple}>
+          <div />
+          <div />
+          <div />
+        </div>
+      </div>,
+      <div className={imageClassName} style={rect}>
+        {tiles}
+      </div>
+    ]
+  }
+  return (
+    <div ref={props.attachRef} className={Styles.container} style={size}>
+      {content}
+    </div>
+  )
+}
+
+function createTiles(name, resolution, onLoad) {
+  return Array(resolution * resolution)
+    .fill()
+    .map((_, i) => {
+      var row = parseInt(i / resolution)
+      var column = i % resolution
+      var key = `_${row}_${column}`
+      var path = `${name}/${key}.png`
+      var tileLoaded = () => onLoad(key)
+      var attachOnLoad = node => {
+        if (node) node.onload = tileLoaded
+      }
+      return <img ref={attachOnLoad} className={Styles.tile} src={path} />
+    })
+}
+
+function applyBehavior(Image) {
+  class Instance extends Component {
+    constructor() {
+      super()
+      this.state = {
+        rect: null,
+        isLoading: true
+      }
+      this._attachRef = this._attachRef.bind(this)
+      this._onTileLoad = this._onTileLoad.bind(this)
+      this._loadingTiles = createTileKeys(IMAGE_RESOLUTION)
+    }
+
+    componentDidMount() {
+      this._updateRect()
+      window.addEventListener('resize', this._updateRect.bind(this))
+    }
+
+    componentWillReceiveProps(nextProps) {
+      if (this.props.name !== nextProps.name) {
+        this.setState({ isLoading: true })
+        this._loadingTiles = createTileKeys(IMAGE_RESOLUTION)
+      }
+    }
+
+    render() {
+      var { rect, isLoading } = this.state
+      var { name, onLoad } = this.props
+      return (
+        <Image
+          attachRef={this._attachRef}
+          rect={rect}
+          isLoading={isLoading}
+          name={name}
+          onLoad={onLoad}
+          onTileLoad={this._onTileLoad}
+        />
+      )
+    }
+
+    _attachRef(node) {
+      this._container = node
+    }
+
+    _updateRect() {
+      this.setState({ rect: null }, () => {
+        var rect = this._container.getBoundingClientRect()
+        this.setState({ rect })
+      })
+    }
+
+    _onTileLoad(key) {
+      delete this._loadingTiles[key]
+      var imageIsLoaded = Object.keys(this._loadingTiles).length === 0
+      if (imageIsLoaded) {
+        this.setState({ isLoading: false })
+      }
+    }
+  }
+
+  return Instance
+}
+
+function createTileKeys(resolution) {
+  return Array(resolution * resolution)
+    .fill()
+    .reduce((keys, _, i) => {
+      var row = parseInt(i / resolution)
+      var column = i % resolution
+      var key = `_${row}_${column}`
+      keys[key] = true
+      return keys
+    }, {})
+}
+
+export default applyBehavior(View)
--- src/page.js
+++ src/page.js
@@ -1,4 +1,5 @@
 import { Component } from 'inferno'
+import Image from './image'
 import Styles from './page.css'
 
 var IMAGES = [
@@ -37,85 +38,30 @@ var IMAGES = [
 ]
 
 function View(props) {
-  return (
-    <div className={Styles.rootContainer}>
-      <div className={Styles.header}>DreamsMakeToys</div>
-      <Image
-        attachRef={props.attachImageRef}
-        size={props.imageSize}
-        name={props.name}
-        isLoading={props.isLoading}
-        onTileLoad={props.onTileLoad}
-      />
-      <Info name={props.name} group={props.group} />
-      <Navbar
-        selectedIndex={props.selectedIndex}
-        previous={props.previous}
-        next={props.next}
-      />
-    </div>
-  )
-}
-
-function Image(props) {
-  var tiles = createTiles(props.name, 5, props.isLoading, props.onTileLoad)
-  return (
-    <div className={Styles.imageContainer} style={props.size}>
-      <div ref={props.attachRef} className={Styles.image}>
-        {tiles}
-      </div>
-    </div>
-  )
-}
-
-function createTiles(name, resolution, isLoading, onLoad) {
-  return Array(resolution * resolution)
-    .fill()
-    .map((_, i) => {
-      var className = `${Styles.tile} ${isLoading ? Styles.loading : ''}`
-      var row = parseInt(i / resolution)
-      var column = i % resolution
-      var key = `_${row}_${column}`
-      var path = `${name}/${key}.png`
-      var tileLoaded = () => onLoad(key)
-      var attachOnLoad = node => (node.onload = tileLoaded)
-      return <img className={className} src={path} ref={attachOnLoad} />
-    })
-}
-
-function createTileKeys(resolution) {
-  return Array(resolution * resolution)
-    .fill()
-    .reduce((keys, _, i) => {
-      var row = parseInt(i / resolution)
-      var column = i % resolution
-      var key = `_${row}_${column}`
-      keys[key] = true
-      return keys
-    }, {})
-}
-
-function Info(props) {
-  return (
-    <div className={Styles.info}>
-      <div className={Styles.label}>{props.name}</div>
-      <div className={Styles.label}>{props.group}</div>
-    </div>
-  )
-}
-
-function Navbar(props) {
   var locator = Array(IMAGES.length)
     .fill()
-    .map((_, i) => <div>{props.selectedIndex == i ? 1 : 0}</div>)
+    .reduce(
+      (result, _, i) => (props.index === i ? result + '1' : result + '0'),
+      ''
+    )
   return (
-    <div className={Styles.navbar}>
-      <div className={Styles.button} onClick={props.previous}>
-        {'<'}
+    <div className={Styles.root}>
+      <div className={Styles.header}>DreamsMakeToys</div>
+      <div className={Styles.image}>
+        <Image name={props.name} />
       </div>
-      <div className={Styles.center}>{locator}</div>
-      <div className={Styles.button} onClick={props.next}>
-        {'>'}
+      <div className={Styles.info}>
+        <div className={Styles.label}>{props.name}</div>
+        <div className={Styles.label}>{props.group}</div>
+      </div>
+      <div className={Styles.footer}>
+        <div className={Styles.button} onClick={props.previous}>
+          {'<'}
+        </div>
+        <div className={Styles.center}>{locator}</div>
+        <div className={Styles.button} onClick={props.next}>
+          {'>'}
+        </div>
       </div>
     </div>
   )
@@ -125,73 +71,37 @@ function applyBehavior(Page) {
   class Instance extends Component {
     constructor() {
       super()
-      this.loadingTiles = createTileKeys(5)
       this.state = {
-        imageSize: null,
-        selectedIndex: 0,
-        loading: true
+        index: 0
       }
-      this._attachImageRef = this._attachImageRef.bind(this)
+      this._previous = this._previous.bind(this)
       this._next = this._next.bind(this)
-      this._prev = this._prev.bind(this)
-      this._onTileLoad = this._onTileLoad.bind(this)
-    }
-
-    componentDidMount() {
-      this._updateImageSize()
-      window.addEventListener('resize', this._updateImageSize.bind(this))
     }
 
     render() {
-      var { imageSize, selectedIndex, loading } = this.state
-      var { name, group } = IMAGES[selectedIndex]
+      var { index } = this.state
+      var { name, group } = IMAGES[index]
       return (
         <Page
-          attachImageRef={this._attachImageRef}
-          imageSize={imageSize}
           name={name}
-          isLoading={loading}
-          onTileLoad={this._onTileLoad}
           group={group}
-          selectedIndex={selectedIndex}
+          index={index}
+          previous={this._previous}
           next={this._next}
-          previous={this._prev}
         />
       )
     }
 
-    _attachImageRef(node) {
-      this._image = node
-    }
-
-    _updateImageSize() {
-      this.setState({ imageSize: null }, () => {
-        var { width } = this._image.getBoundingClientRect()
-        var imageSize = { width, height: width }
-        this.setState({ imageSize })
-      })
+    _previous() {
+      var { index } = this.state
+      var nextIndex = (index + 7) % IMAGES.length
+      this.setState({ index: nextIndex })
     }
 
     _next() {
-      var selectedIndex = this.state.selectedIndex + 1
-      selectedIndex = selectedIndex % IMAGES.length
-      this.loadingTiles = createTileKeys(5)
-      this.setState({ selectedIndex, loading: true })
-    }
-
-    _prev() {
-      var selectedIndex = this.state.selectedIndex + (IMAGES.length - 1)
-      selectedIndex = selectedIndex % IMAGES.length
-      this.loadingTiles = createTileKeys(5)
-      this.setState({ selectedIndex, loading: true })
-    }
-
-    _onTileLoad(key) {
-      delete this.loadingTiles[key]
-      var imageIsLoaded = Object.keys(this.loadingTiles).length == 0
-      if (imageIsLoaded) {
-        this.setState({ loading: false })
-      }
+      var { index } = this.state
+      var nextIndex = (index + 1) % IMAGES.length
+      this.setState({ index: nextIndex })
     }
   }
 
